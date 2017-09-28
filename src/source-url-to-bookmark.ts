67cf46cec17ac968193a3aa3d7b679a3a4096264
Gabor Zoltan Toth <latotty@codingsans.com>
2017-09-28 20:58:26 +0200

    feat(bookmark): better string

--- src/source-url-to-bookmark.ts
+++ src/source-url-to-bookmark.ts
@@ -1,3 +1,3 @@
 export function sourceUrlToBookmark(sourceUrl: string): string {
-  return `(a=>{var b=document.createElement('script');b.src=a,b.type='text/javascript',(document.head||document.getElementsByTagName('head')[0]).appendChild(b)})('${sourceUrl}');`;
+  return `javascript:(a=>{var b=document.createElement('script');b.src=a,b.type='text/javascript',(document.head||document.getElementsByTagName('head')[0]).appendChild(b)})('${sourceUrl}');`;
 }
